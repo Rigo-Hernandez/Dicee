9c5506ed3e07ecdbb640c8419c52e0fd6a80c68c
Rigo Hernandez <rigohernandez@Rigos-iMac.attlocal.net>
2019-05-21 22:21:13 -0500

    first commit

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Rigo Hernandez on 5/21/19.
+//  Copyright © 2019 Rigo Hernandez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
